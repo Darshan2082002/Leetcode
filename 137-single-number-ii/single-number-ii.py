b0a41ae1cc1dedc91e8006590d040fa4a8c5de37
Darshan B <87748126+Darshan2082002@users.noreply.github.com>
2026-09-21 00:33:00 +0530

    Time: 3 ms (71.16%) | Memory: 20.6 MB (63.35%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        one=0
+        two=0
+        for i in nums:
+            one=(one^i)&~two
+            two=(two^i)&~one
+        return one 
+          
